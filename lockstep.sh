c9b4e30fc14ba384f2c1b637e064b22a0c4ce651
Henry Marshall <henry@isagoddamn.ninja>
2017-04-09 15:53:49 -0400

    Setup repository before running lockstep

--- lockstep.sh
+++ lockstep.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
 
-if git status > /dev/null
+# Initialize a repo if necessary & check that no files are already in existance
+if git status > /dev/null 2>&1
 then
-  if ! git status | grep -o "nothing to commit" > /dev/null
+  if git status | grep -o "nothing to commit" > /dev/null
   then
+    starting_branch=`git branch | grep -Po "(?<=\*\s).+"`
+    screencast_branch="screencast_`date +"%Y-%m-%dT%H-%M-%S"`"
+    git checkout -b $screencast_branch
+  else
     echo "Please, commit your changes or stash them before running lockstep"
     exit
   fi
 else
-  echo "Initializing a git repository"
-  git init
+  echo "You are not in a git repository."
+  echo "Would you like to create one here? (Y/n)"
+  read should_create_repo
+  if [ "$should_create_repo" != "y" ] && [ "$should_create_repo" != "Y" ]
+  then
+    git init
+    git add .
+    git commit -m "Lockstep commit at `date +"%Y-%m-%d %H:%M:%S"`" > /dev/null
+    
+    starting_branch=""
+    screencast_branch="master"
+  else
+    exit
+  fi
 fi
 
 echo "Let's do this!"
